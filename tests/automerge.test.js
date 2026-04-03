1f382e3091e2f56543358e5902a8cee66e27b65f
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-03 19:20:54 +0700

    chore: debug

--- tests/automerge.test.js
+++ tests/automerge.test.js
@@ -1,13 +1,17 @@
 import { describe, expect, test } from '@jest/globals'
+import { z } from zod;
 
 import { WdkAssetSchema } from '@tetherto/wdk-asset-registry'
 
-const changedFiles = process.env.CHANGED_JSON_FILES || ''
-
-console.log("============", changedFiles)
+const ChangeFilesSchema = z.object({CHANGED_JSON_FILES: z.string()}).transform(str => {
+  return val.split('\n').filter(e => !e)
+})
 
 describe('CI: Automerge', () => {
+  const { CHANGED_JSON_FILES: changedFiles } = ChangeFilesSchema.parse(process.env);
+
   test("should be valid object following the wdk asset schema", () => {
+    console.log(changedFiles)
     expect(true).toBe(true)
   })
 })
