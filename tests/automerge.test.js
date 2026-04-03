c767faa7e8041db7c9f64adae67b9a3396b0629a
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-03 19:28:59 +0700

    fix: filter

--- tests/automerge.test.js
+++ tests/automerge.test.js
@@ -5,12 +5,12 @@ import { WdkAssetSchema } from '@tetherto/wdk-asset-registry'
 
 const ChangeFilesSchema = z.object({
   CHANGED_JSON_FILES: z.string().transform(str => {
-    return str.split('\n').filter(e => !e)
+    return str.split('\n').filter(e => !!e)
   })
 })
 
 describe('CI: Automerge', () => {
-  const { CHANGED_JSON_FILES: changedFiles } = ChangeFilesSchema.parse(process.env);
+  const { CHANGED_JSON_FILES: changedFiles } = ChangeFilesSchema.parse(process.env)
 
   test("should be valid object following the wdk asset schema", () => {
     console.log(changedFiles)
