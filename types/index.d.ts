fa7835999086b335c57ceb5e25effd38c6e52a42
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-03 16:08:57 +0700

    chore: first commit

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./src/wallet-asset.js";
+export type WdkAsset = import("./src/wallet-asset.js").WdkAsset;
---
+++ types/src/wallet-asset.d.ts
@@ -0,0 +1,34 @@
+/**
+ * @typedef {z.infer<typeof WdkAssetSchema>} WdkAsset - Type representing a validated WDK asset object.
+ */
+export const WdkAssetSchema: z.ZodObject<{
+    address: z.ZodString;
+    symbol: z.ZodString;
+    name: z.ZodString;
+    decimals: z.ZodNumber;
+    chainId: z.ZodNumber;
+    logoURI: z.ZodURL;
+    tags: z.ZodOptional<z.ZodArray<z.ZodUnion<readonly [z.ZodString, z.ZodObject<{
+        name: z.ZodString;
+        description: z.ZodString;
+    }, z.core.$strip>]>>>;
+    extensions: z.ZodOptional<z.ZodRecord<z.ZodString, z.ZodUnknown>>;
+}, z.core.$strip>;
+export const WdkAssetJsonSchema: z.core.ZodStandardJSONSchemaPayload<z.ZodObject<{
+    address: z.ZodString;
+    symbol: z.ZodString;
+    name: z.ZodString;
+    decimals: z.ZodNumber;
+    chainId: z.ZodNumber;
+    logoURI: z.ZodURL;
+    tags: z.ZodOptional<z.ZodArray<z.ZodUnion<readonly [z.ZodString, z.ZodObject<{
+        name: z.ZodString;
+        description: z.ZodString;
+    }, z.core.$strip>]>>>;
+    extensions: z.ZodOptional<z.ZodRecord<z.ZodString, z.ZodUnknown>>;
+}, z.core.$strip>>;
+/**
+ * - Type representing a validated WDK asset object.
+ */
+export type WdkAsset = z.infer<typeof WdkAssetSchema>;
+import { z } from 'zod';
