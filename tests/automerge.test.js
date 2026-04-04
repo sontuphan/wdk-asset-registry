f5eb009afd5c6a1191afb34224cd939c2af4a433
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-04 10:07:56 +0700

    fix: add prescript validation

--- tests/automerge.test.js
+++ tests/automerge.test.js
@@ -3,21 +3,14 @@ import { z } from 'zod'
 
 import { WdkAssetSchema } from '@tetherto/wdk-asset-registry'
 
-const ChangeFilesSchema = z.object({
-  CHANGED_JSON_FILES: z.string().transform(str => {
-    return str.split('\n').filter(e => !!e)
-  })
+const ChangedFileSchema = z.object({
+  CHANGED_JSON_FILE: z.string()
 })
 
 describe('CI: Automerge', () => {
-  const { CHANGED_JSON_FILES: changedFiles } = ChangeFilesSchema.parse(process.env)
-
-  test('should change exactly one file', () => {
-    expect(changedFiles).toHaveLength(1)
-  })
+  const { CHANGED_JSON_FILE: changedFile } = ChangedFileSchema.parse(process.env)
 
   test("should be valid object against the wdk asset schema", async () => {
-    const [changedFile] = changedFiles
     const { default: entry } = await import(`../${changedFile}`, { with: { type: 'json' } })
 
     const data = WdkAssetSchema.parse(entry)
