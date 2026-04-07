95089f977f5497d0ee1eb4f6100d59163c495a84
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-07 07:55:40 +0700

    fix: separate WdkAsset and WdkAssetList

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
 'use strict'
 
 /** @typedef {import('./src/wallet-asset.js').WdkAsset} WdkAsset */
+/** @typedef {import('./src/wallet-asset.js').WdkAssetList} WdkAssetList */
 /** @typedef {import('./src/wallet-asset-util.js').WdkAsset} WdkAssetPagination */
 
 export * from './src/wallet-asset.js'
