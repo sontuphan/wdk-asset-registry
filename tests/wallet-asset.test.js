6fb3642a8429477a3d6d0a373b46fc257529cfff
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-08 21:34:58 +0700

    fix: only deploy when there exist actions on main

--- tests/wallet-asset.test.js
+++ tests/wallet-asset.test.js
@@ -1,4 +1,6 @@
 import { describe, expect, test } from '@jest/globals'
+import { mkdirSync, readdirSync, readFileSync, rmSync, writeFileSync } from 'fs'
+
 import {
   WdkAssetJsonSchema,
   WdkAssetListJsonSchema,
@@ -59,5 +61,17 @@ describe('wallet-asset', () => {
     expect(WdkAssetListJsonSchema.type).toBe('array')
     expect(WdkAssetListJsonSchema.items.type).toBe('object')
     expect(WdkAssetListJsonSchema.items.properties.address.type).toBe('string')
+  })
+
+  test('should validate all the current assets', () => {
+    const files = readdirSync('assets')
+
+    for (const file of files) {
+      const raw = readFileSync(`assets/${file}`, 'utf-8')
+      const data = JSON.parse(raw)
+      const assets = WdkAssetListSchema.parse(data)
+
+      expect(assets.length).toBeGreaterThanOrEqual(1)
+    }
   })
 })
