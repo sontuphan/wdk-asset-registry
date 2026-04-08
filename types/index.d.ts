00bd305287a883fd634245440e86b78e03a429ff
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-08 20:35:20 +0700

    fix: disable open contributions

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,3 @@ export * from "./src/wallet-asset.js";
 export * from "./src/wallet-asset-util.js";
 export type WdkAsset = import("./src/wallet-asset.js").WdkAsset;
 export type WdkAssetList = import("./src/wallet-asset.js").WdkAssetList;
-export type WdkAssetPagination = import("./src/wallet-asset-util.js").WdkAsset;
