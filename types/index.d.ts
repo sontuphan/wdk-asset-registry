8f9a733ef133ea831570140170a35114cfa4e2a6
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-13 17:54:48 +0700

    fix: update jsdocs

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,4 +3,5 @@ export * from "./src/wdk-base-asset-registry.js";
 export * from "./src/wdk-token-asset-registry.js";
 export { WdkBaseAssetRegistry as default } from "./src/wdk-base-asset-registry.js";
 export type BaseAssetFilter = import("./src/wdk-base-asset-registry.js").BaseAssetFilter;
-export type TokenAsset = import("./src/wdk-asset-schema.js").TokenAsset;
+export type BaseAsset = any;
+export type TokenAsset = any;
