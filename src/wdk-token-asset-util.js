5ab010a2764376fe77811f120d1191d59c225b74
Tu <tu.phan@tether.io>
2026-05-11 11:22:58 +0700

    Update src/wdk-token-asset-util.js
    Co-authored-by: Davide Casale <davi0kprogramsthings@gmail.com>

--- src/wdk-token-asset-util.js
+++ src/wdk-token-asset-util.js
@@ -52,7 +52,8 @@ export function fromUniswapToken (token) {
 
 /**
  * Convert a Uniswap Token Lists token array into `TokenAsset[]`.
- * See https://tokenlists.org/.
+ * 
+ * @see https://tokenlists.org/
  *
  * @param {UniswapTokenInfo[]} tokens - Source token entries.
  * @returns {TokenAsset[]} The normalized token assets.
